bdad08d4eeaad09ff5b260eea523f07bd1a2a0c5
Treri <treri.liu@gmail.com>
2015-01-20 10:33:42 +0800

    update module

--- angular-require.js
+++ angular-require.js
@@ -58,24 +58,23 @@
     var app = module.apply(angular, args);
     if (args[1].indexOf('ngRequire') < 0) {
       return app;
-    } else {
-      return app.config([
-        '$controllerProvider',
-        '$compileProvider',
-        '$filterProvider',
-        '$provide',
-        function($controllerProvider, $compileProvider, $filterProvider, $provide) {
-          app.controller = $controllerProvider.register;
-          app.directive = $compileProvider.directive;
-          app.filter = $filterProvider.register;
-          app.factory = $provide.factory;
-          app.service = $provide.service;
-          app.provider = $provide.provider;
-          app.value = $provide.value;
-          app.constant = $provide.constant;
-          app.decorator = $provide.decorator;
-        }
-      ]);
     }
+    return app.config([
+      '$controllerProvider',
+      '$compileProvider',
+      '$filterProvider',
+      '$provide',
+      function($controllerProvider, $compileProvider, $filterProvider, $provide) {
+        app.controller = $controllerProvider.register;
+        app.directive = $compileProvider.directive;
+        app.filter = $filterProvider.register;
+        app.factory = $provide.factory;
+        app.service = $provide.service;
+        app.provider = $provide.provider;
+        app.value = $provide.value;
+        app.constant = $provide.constant;
+        app.decorator = $provide.decorator;
+      }
+    ]);
   };
 })();
